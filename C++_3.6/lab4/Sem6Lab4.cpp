3e77ceb8ac9ea15ced281cb1716c458d12616c97
iSergoff <iSergoff@users.noreply.github.com>
2021-06-07 16:24:17 +0300

    remade lab 4 C++_3.6

--- C++_3.6/lab4/Sem6Lab4.cpp
+++ C++_3.6/lab4/Sem6Lab4.cpp
@@ -1,221 +1,205 @@
-#include <iostream>
-#include <cstring>
+#include <stdio.h>
 #include <string>
-#include <random>
+#include <string.h>
+#include <iostream>
+#include <cstdint>
+#include <sstream>
 #include <chrono>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
- 
-class SHA2{
 
-    protected:
-        #define SHA2_SHIFT_R(x, n)    (x >> n)
-        #define SHA2_ROTATE_R(x, n)   ((x >> n) | (x << ((sizeof(x) << 3) - n)))
-        #define SHA2_ROTATE_L(x, n)   ((x << n) | (x >> ((sizeof(x) << 3) - n)))
-        #define SHA2_CH(x, y, z)  ((x & y) ^ (~x & z))
-        #define SHA2_MAJ(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
-        #define SHA2_SIGMA_0(x) (SHA2_ROTATE_R(x,  2) ^ SHA2_ROTATE_R(x, 13) ^ SHA2_ROTATE_R(x, 22))
-        #define SHA2_SIGMA_1(x) (SHA2_ROTATE_R(x,  6) ^ SHA2_ROTATE_R(x, 11) ^ SHA2_ROTATE_R(x, 25))
-        #define SHA2_DELTA_0(x) (SHA2_ROTATE_R(x,  7) ^ SHA2_ROTATE_R(x, 18) ^ SHA2_SHIFT_R(x,  3))
-        #define SHA2_DELTA_1(x) (SHA2_ROTATE_R(x, 17) ^ SHA2_ROTATE_R(x, 19) ^ SHA2_SHIFT_R(x, 10))
+class SHA512 {
+private:
+	typedef unsigned long long uint64;
 
-        #define SHA2_UNPACK32(x, str){              \
-            *((str) + 3) = (uint8) ((x)      );       \
-            *((str) + 2) = (uint8) ((x) >>  8);       \
-            *((str) + 1) = (uint8) ((x) >> 16);       \
-            *((str) + 0) = (uint8) ((x) >> 24);       \
-        }
+	const uint64 hPrime[8] = { 0x6a09e667f3bcc908ULL,
+								0xbb67ae8584caa73bULL,
+								0x3c6ef372fe94f82bULL,
+								0xa54ff53a5f1d36f1ULL,
+								0x510e527fade682d1ULL,
+								0x9b05688c2b3e6c1fULL,
+								0x1f83d9abfb41bd6bULL,
+								0x5be0cd19137e2179ULL
+	};
 
-        #define SHA2_PACK32(str, x){                \
-            *(x) =   ((uint32) *((str) + 3)   )       \
-                | ((uint32) *((str) + 2) <<  8)       \
-                | ((uint32) *((str) + 1) << 16)       \
-                | ((uint32) *((str) + 0) << 24);      \
-        }
+	const uint64 k[80] = { 0x428a2f98d728ae22ULL, 0x7137449123ef65cdULL, 0xb5c0fbcfec4d3b2fULL, 0xe9b5dba58189dbbcULL, 0x3956c25bf348b538ULL,
+			  0x59f111f1b605d019ULL, 0x923f82a4af194f9bULL, 0xab1c5ed5da6d8118ULL, 0xd807aa98a3030242ULL, 0x12835b0145706fbeULL,
+			  0x243185be4ee4b28cULL, 0x550c7dc3d5ffb4e2ULL, 0x72be5d74f27b896fULL, 0x80deb1fe3b1696b1ULL, 0x9bdc06a725c71235ULL,
+			  0xc19bf174cf692694ULL, 0xe49b69c19ef14ad2ULL, 0xefbe4786384f25e3ULL, 0x0fc19dc68b8cd5b5ULL, 0x240ca1cc77ac9c65ULL,
+			  0x2de92c6f592b0275ULL, 0x4a7484aa6ea6e483ULL, 0x5cb0a9dcbd41fbd4ULL, 0x76f988da831153b5ULL, 0x983e5152ee66dfabULL,
+			  0xa831c66d2db43210ULL, 0xb00327c898fb213fULL, 0xbf597fc7beef0ee4ULL, 0xc6e00bf33da88fc2ULL, 0xd5a79147930aa725ULL,
+			  0x06ca6351e003826fULL, 0x142929670a0e6e70ULL, 0x27b70a8546d22ffcULL, 0x2e1b21385c26c926ULL, 0x4d2c6dfc5ac42aedULL,
+			  0x53380d139d95b3dfULL, 0x650a73548baf63deULL, 0x766a0abb3c77b2a8ULL, 0x81c2c92e47edaee6ULL, 0x92722c851482353bULL,
+			  0xa2bfe8a14cf10364ULL, 0xa81a664bbc423001ULL, 0xc24b8b70d0f89791ULL, 0xc76c51a30654be30ULL, 0xd192e819d6ef5218ULL,
+			  0xd69906245565a910ULL, 0xf40e35855771202aULL, 0x106aa07032bbd1b8ULL, 0x19a4c116b8d2d0c8ULL, 0x1e376c085141ab53ULL,
+			  0x2748774cdf8eeb99ULL, 0x34b0bcb5e19b48a8ULL, 0x391c0cb3c5c95a63ULL, 0x4ed8aa4ae3418acbULL, 0x5b9cca4f7763e373ULL,
+			  0x682e6ff3d6b2b8a3ULL, 0x748f82ee5defb2fcULL, 0x78a5636f43172f60ULL, 0x84c87814a1f0ab72ULL, 0x8cc702081a6439ecULL,
+			  0x90befffa23631e28ULL, 0xa4506cebde82bde9ULL, 0xbef9a3f7b2c67915ULL, 0xc67178f2e372532bULL, 0xca273eceea26619cULL,
+			  0xd186b8c721c0c207ULL, 0xeada7dd6cde0eb1eULL, 0xf57d4f7fee6ed178ULL, 0x06f067aa72176fbaULL, 0x0a637dc5a2c898a6ULL,
+			  0x113f9804bef90daeULL, 0x1b710b35131c471bULL, 0x28db77f523047d84ULL, 0x32caab7b40c72493ULL, 0x3c9ebe0a15c9bebcULL,
+			  0x431d67c49c100d4cULL, 0x4cc5d4becb3e42b6ULL, 0x597f299cfc657e2aULL, 0x5fcb6fab3ad6faecULL, 0x6c44198c4a475817ULL };
 
-        SHA2(){
-            initHashesAndLen();
-        }
+	static const unsigned int SEQUENCE_LEN = (1024 / 64);
 
-        typedef unsigned char uint8;
-        typedef unsigned int uint32;
-        static const unsigned int SHA2_BLOCK_SIZE = (512 / 8);
-        static const unsigned int HASH_SIZE = (256 / 8);
-        unsigned int crypted_message_len;
-        unsigned int not_crypted_message_len;
-        unsigned char m_block[2 * SHA2_BLOCK_SIZE] = {0};
-        uint32 hash_val[8];
+	uint64** preprocess(const unsigned char* input, size_t& nBuffer);
+	void appendLen(uint64 mLen, uint64 mp, uint64& lo, uint64& hi);
+	void process(uint64** buffer, size_t nBuffer, uint64* h);
+	std::string digest(uint64* h);
+	void freeBuffer(uint64** buffer, size_t nBuffer);
 
-        const unsigned int SHA2_KEYS[64] = {
-            0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-            0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-            0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-            0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-            0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-            0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-            0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-            0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-            0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-            0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-            0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-            0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-            0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-            0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-            0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-        };
+public:
+	std::string hash(const std::string input);
 
-        
-        void initHashesAndLen(){
-            
-            hash_val[0] = 0x6a09e667;
-            hash_val[1] = 0xbb67ae85;
-            hash_val[2] = 0x3c6ef372;
-            hash_val[3] = 0xa54ff53a;
-            hash_val[4] = 0x510e527f;
-            hash_val[5] = 0x9b05688c;
-            hash_val[6] = 0x1f83d9ab;
-            hash_val[7] = 0x5be0cd19;
-
-            not_crypted_message_len = 0;
-            crypted_message_len = 0;
-        }
-
-
-        void separateMessage(const unsigned char* message, unsigned int len){
-            unsigned int block_amount;
-            unsigned int rest_message_len, not_in_blocks_len;
-            const unsigned char* leftover_message;
-            not_in_blocks_len = len < SHA2_BLOCK_SIZE ? len : SHA2_BLOCK_SIZE;
-
-            memcpy(&m_block[not_crypted_message_len], message, not_in_blocks_len);
-
-            if (not_crypted_message_len + len < SHA2_BLOCK_SIZE) {
-                not_crypted_message_len += len;
-                return;
-            }
-
-            rest_message_len = len - not_in_blocks_len;
-            block_amount = rest_message_len / SHA2_BLOCK_SIZE;
-            leftover_message = message + not_in_blocks_len;
-
-            computeHash(m_block, 1);
-            computeHash(leftover_message, block_amount);
-            not_in_blocks_len = rest_message_len % SHA2_BLOCK_SIZE;
-            memcpy(m_block, &leftover_message[block_amount << 6], not_in_blocks_len);
-            not_crypted_message_len = not_in_blocks_len;
-            crypted_message_len += (block_amount + 1) << 6;
-        }
-
-        
-        void finalHash(unsigned char* hash){
-            unsigned int block_amount;
-            unsigned int rest_block_bytes;
-            unsigned int message_len_bits;
-
-            block_amount = (1 + ((SHA2_BLOCK_SIZE - 9) < (not_crypted_message_len % SHA2_BLOCK_SIZE)));
-            message_len_bits = (crypted_message_len + not_crypted_message_len) << 3;
-            rest_block_bytes = block_amount << 6;
-            memset(m_block + not_crypted_message_len, 0, rest_block_bytes - not_crypted_message_len);
-            m_block[not_crypted_message_len] = 0x80;
-
-            SHA2_UNPACK32(message_len_bits, m_block + rest_block_bytes - 4);
-            computeHash(m_block, block_amount);
-
-            for (int i = 0 ; i < 8; i++) {
-                SHA2_UNPACK32(hash_val[i], &hash[i << 2]);
-            }
-        }
-        
-        
-        void computeHash(const unsigned char* message, unsigned int block_amount){
-            
-            uint32 w[64];
-            uint32 hash_val_copy[8];
-            uint32 temp1, temp2;
-
-            const unsigned char* sub_block;
-
-            for (int i = 0; i < block_amount; i++) {
-                sub_block = message + (i << 6);
-                for (int j = 0; j < 16; j++) {
-                    SHA2_PACK32(&sub_block[j << 2], &w[j]);
-                }
-
-                for (int j = 16; j < 64; j++) {
-                    w[j] =  SHA2_DELTA_1(w[j - 2]) + w[j -  7] + SHA2_DELTA_0(w[j - 15]) + w[j - 16];
-                }
-
-                for (int j = 0; j < 8; j++) {
-                    hash_val_copy[j] = hash_val[j];
-                }
-
-                for (int j = 0; j < 64; j++) {
-                    temp1 = hash_val_copy[7] + SHA2_SIGMA_1(hash_val_copy[4]) + SHA2_CH(hash_val_copy[4], hash_val_copy[5], hash_val_copy[6])
-                        + SHA2_KEYS[j] + w[j];
-                    temp2 = SHA2_SIGMA_0(hash_val_copy[0]) + SHA2_MAJ(hash_val_copy[0], hash_val_copy[1], hash_val_copy[2]);
-                    hash_val_copy[7] = hash_val_copy[6];
-                    hash_val_copy[6] = hash_val_copy[5];
-                    hash_val_copy[5] = hash_val_copy[4];
-                    hash_val_copy[4] = hash_val_copy[3] + temp1;
-                    hash_val_copy[3] = hash_val_copy[2];
-                    hash_val_copy[2] = hash_val_copy[1];
-                    hash_val_copy[1] = hash_val_copy[0];
-                    hash_val_copy[0] = temp1 + temp2;
-                }
-
-                for (int j = 0; j < 8; j++) {
-                    hash_val[j] += hash_val_copy[j];
-                }
-            }
-        }
-
-        friend string sha256(string);
+	SHA512();
+	~SHA512();
 };
 
+#define Ch(x,y,z) ((x&y)^(~x&z))
+#define Maj(x,y,z) ((x&y)^(x&z)^(y&z))
+#define RotR(x, n) ((x>>n)|(x<<((sizeof(x)<<3)-n)))
+#define Sig0(x) ((RotR(x, 28))^(RotR(x,34))^(RotR(x, 39)))
+#define Sig1(x) ((RotR(x, 14))^(RotR(x,18))^(RotR(x, 41)))
+#define sig0(x) (RotR(x, 1)^RotR(x,8)^(x>>7))
+#define sig1(x) (RotR(x, 19)^RotR(x,61)^(x>>6))
 
-string sha256(string input){
-    unsigned char hash[SHA2::HASH_SIZE] = {0};
-    SHA2 sha_obj;
+typedef unsigned long long uint64;
 
-    sha_obj.separateMessage( (unsigned char*)input.c_str(), input.length());
-    sha_obj.finalHash(hash);
-    
-    char final_hash[2 * SHA2::HASH_SIZE + 1] = {0};
-    final_hash[2 * SHA2::HASH_SIZE] = 0;
-
-    for (int i = 0; i < SHA2::HASH_SIZE; i++)
-        sprintf(final_hash + i * 2, "%02x", hash[i]);
-
-    return string(final_hash);
+SHA512::SHA512() {
 }
 
+SHA512::~SHA512() {
+}
+
+std::string SHA512::hash(const std::string input) {
+	size_t nBuffer; 
+	uint64** buffer; 
+	uint64* h = new uint64[8];
+	buffer = preprocess((unsigned char*)input.c_str(), nBuffer);
+	process(buffer, nBuffer, h);
+	freeBuffer(buffer, nBuffer);
+	return digest(h);
+}
+
+uint64** SHA512::preprocess(const unsigned char* input, size_t& nBuffer) {
+	size_t mLen = strlen((const char*)input);
+	size_t kLen = (895 - (mLen * 8)) % 1024;
+	nBuffer = (mLen * 8 + 1 + kLen + 128) / 1024;
+	uint64** buffer = new uint64 * [nBuffer];
+	for (size_t i = 0; i < nBuffer; i++) {
+		buffer[i] = new uint64[SEQUENCE_LEN];
+	}
+	for (size_t i = 0; i < nBuffer; i++) {
+		for (size_t j = 0; j < SEQUENCE_LEN; j++) {
+			uint64 in = 0x0ULL;
+			for (size_t k = 0; k < 8; k++) {
+				if (i * 128 + j * 8 + k < mLen) {
+					in = in << 8 | (uint64)input[i * 128 + j * 8 + k];
+				}
+				else if (i * 128 + j * 8 + k == mLen) {
+					in = in << 8 | 0x80ULL;
+				}
+				else {
+					in = in << 8 | 0x0ULL;
+				}
+			}
+			buffer[i][j] = in;
+		}
+	}
+	appendLen(mLen, 8, buffer[nBuffer - 1][SEQUENCE_LEN - 1], buffer[nBuffer - 1][SEQUENCE_LEN - 2]);
+	return buffer;
+}
+
+void SHA512::process(uint64** buffer, size_t nBuffer, uint64* h) {
+	uint64 s[8];
+	uint64 w[80];
+	memcpy(h, hPrime, 8 * sizeof(uint64));
+	for (size_t i = 0; i < nBuffer; i++) {
+		memcpy(w, buffer[i], 16 * sizeof(uint64));
+		for (size_t j = 16; j < 80; j++) {
+			w[j] = w[j - 16] + sig0(w[j - 15]) + w[j - 7] + sig1(w[j - 2]);
+		}
+		memcpy(s, h, 8 * sizeof(uint64));
+		for (size_t j = 0; j < 80; j++) {
+			uint64 temp1 = s[7] + Sig1(s[4]) + Ch(s[4], s[5], s[6]) + k[j] + w[j];
+			uint64 temp2 = Sig0(s[0]) + Maj(s[0], s[1], s[2]);
+
+			s[7] = s[6];
+			s[6] = s[5];
+			s[5] = s[4];
+			s[4] = s[3] + temp1;
+			s[3] = s[2];
+			s[2] = s[1];
+			s[1] = s[0];
+			s[0] = temp1 + temp2;
+		}
+		for (size_t j = 0; j < 8; j++) {
+			h[j] += s[j];
+		}
+	}
+
+}
+
+void SHA512::appendLen(uint64 mLen, uint64 mp, uint64& lo, uint64& hi) {
+	uint64_t u1 = (mLen & 0xffffffff);
+	uint64_t v1 = (mp & 0xffffffff);
+	uint64_t t = (u1 * v1);
+	uint64_t w3 = (t & 0xffffffff);
+	uint64_t k = (t >> 32);
+
+	mLen >>= 32;
+	t = (mLen * v1) + k;
+	k = (t & 0xffffffff);
+	uint64_t w1 = (t >> 32);
+
+	mp >>= 32;
+	t = (u1 * mp) + k;
+	k = (t >> 32);
+
+	hi = (mLen * mp) + w1 + k;
+	lo = (t << 32) + w3;
+}
+
+std::string SHA512::digest(uint64* h) {
+	std::stringstream ss;
+	for (size_t i = 0; i < 8; i++) {
+		ss << std::hex << std::setw(16) << std::setfill('0') << h[i];
+	}
+	delete[] h;
+	return ss.str();
+
+}
+
+void SHA512::freeBuffer(uint64** buffer, size_t nBuffer) {
+	for (size_t i = 0; i < nBuffer; i++) {
+		delete[] buffer[i];
+	}
+
+	delete[] buffer;
+}
 
 int main(){
-    mt19937 engine(time(0));
-
-    ofstream res_file;
-    res_file.open("res.txt", ios::out);
-
-    string test_str = "Hello world Hello world Hello wo";
-
+    ofstream output;
+    output.open("output.txt", ios::out);
+    string input = "Just take a look at this string ";
+	SHA512 sha512;
     chrono::high_resolution_clock::time_point start;
     chrono::high_resolution_clock::time_point end;
     chrono::duration<double, milli> milli_diff;
-    for(int i = 0; i < 5; i++){
-        
+    for (int i = 0; i < 10; i++) {
         start = chrono::high_resolution_clock::now();
-        
-        string ready_hash = sha256(test_str);
-
+		string hash = sha512.hash(input);
         end = chrono::high_resolution_clock::now();
         milli_diff = end - start;
-
-        res_file << test_str.size() << " - " << milli_diff.count() << " - " << test_str << " - " << ready_hash << endl;
-
-        test_str += test_str;
+        output << "Размер строки: " << input.size() << ",\nсама строка: \"" << input << "\",\nвремя обработки: " << milli_diff.count() << " мс,\nхэш: " << hash << endl << endl;
+		input += input;
     }
-    
-    res_file.close();
+
+	//Для сравнения со стандартной реализацией
+	string hash = sha512.hash("0T]Gk4GxsCCt83${#,7K{qTyF@VR}^+M}jeB 7{#M8f:#Xd:=d");
+	output << hash << endl;
+
+    output.close();
+    return 0;
 }
